0a43aafef4bcb6b261b4aad1c50d067dfbdc1421
LeeJunyeol <chljy33@naver.com>
2017-07-08 18:55:40 +0900

    Revert "C_LeeJunYeol 카테고리등록/수정/삭제"
    This reverts commit 592a4dd0292dec91440d0a2af77fea211da53b5b.

--- reservation-service/src/main/java/kr/or/connect/jy/dao/CategorySqls.java
+++
@@ -1,9 +0,0 @@
-package kr.or.connect.jy.dao;
-
-public class CategorySqls {
-    final static String SELECT_ALL = "select id, name from category";
-    final static String SELECT_BY_ID = "select id, name from category where id = :id";
-    final static String UPDATE_BY_ID = "update category set name = :name where id = :id";
-    final static String DELETE_BY_ID = "delete from category where id = :id";
-    //final static String SELECT = "select id, name, email, passwd from member order by id desc limit :start, :count";
-}
