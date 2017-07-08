0e03fe1607e0ad89769f22eb60a7f046b0dacfb4
류성탁 <tak@lyuseongtag-ui-MacBook-Air.local>
2017-07-08 20:15:27 +0900

    add update api

--- reservation-service/src/main/java/kr/or/connect/reservation/controller/CategoryController.java
+++ reservation-service/src/main/java/kr/or/connect/reservation/controller/CategoryController.java
@@ -1,13 +1,11 @@
 package kr.or.connect.reservation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.or.connect.reservation.domain.Category;
 import kr.or.connect.reservation.service.CategoryService;
--- reservation-service/src/main/java/kr/or/connect/reservation/controller/CategoryRestController.java
+++ reservation-service/src/main/java/kr/or/connect/reservation/controller/CategoryRestController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.or.connect.reservation.domain.Category;
 import kr.or.connect.reservation.service.CategoryService;
 
 @RestController
@@ -21,5 +24,11 @@ public class CategoryRestController {
 	@ResponseStatus(HttpStatus.OK)
 	int deleteCategory(@PathVariable("id") Long id) {
 		return service.delete(id);
+	}
+	
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	int updateCategory(@RequestBody Category category) {
+		return service.update(category);
 	}
 }
