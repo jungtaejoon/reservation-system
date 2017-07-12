417e71c8da6e4ce4ba24da092690eb7d82954903
류성탁 <tak@lyuseongtag-ui-MacBook-Air.local>
2017-07-12 23:36:05 +0900

    Add Product Dto, Domain

---
+++ reservation-service/src/main/java/kr/or/connect/reservation/domain/Product.java
@@ -0,0 +1,79 @@
+package kr.or.connect.reservation.domain;
+
+import java.sql.Date;
+
+public class Product {
+	private long id;
+	private long categoryId;
+	private String name;
+	private String description;
+	private String placeName;
+	private String fileName;
+	private String saveFileName;
+
+	public Product() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPlaceName() {
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", categoryId=" + categoryId + ", name=" + name + ", description=" + description
+				+ ", placeName=" + placeName + ", fileName=" + fileName + ", saveFileName=" + saveFileName + "]";
+	}
+
+}
