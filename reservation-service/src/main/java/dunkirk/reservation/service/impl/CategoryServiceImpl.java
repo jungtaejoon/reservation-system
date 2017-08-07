d4c33653bd56d88ee43ee90f56fb7b55328120dd
won <smilesj@outlook.kr>
2017-08-07 19:02:45 +0900

    서비스 에러 수정

--- reservation-service/src/main/java/dunkirk/reservation/service/impl/CategoryServiceImpl.java
+++ reservation-service/src/main/java/dunkirk/reservation/service/impl/CategoryServiceImpl.java
@@ -3,11 +3,13 @@ package dunkirk.reservation.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dunkirk.reservation.dao.CategoryDao;
 import dunkirk.reservation.domain.Category;
 import dunkirk.reservation.service.CategoryService;
 
+@Service
 public class CategoryServiceImpl implements CategoryService{
 
 	private CategoryDao categoryDao;
