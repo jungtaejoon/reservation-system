cf0c29f7d8cd31cf12f1b8d4eb70ff5f04638830
jungtaejoon <jungtaejoon@naver.com>
2017-08-08 12:23:42 +0900

    테스트

--- reservation-service/src/main/java/dunkirk/reservation/dao/impl/ProductDaoImpl.java
+++ reservation-service/src/main/java/dunkirk/reservation/dao/impl/ProductDaoImpl.java
@@ -7,23 +7,23 @@ import javax.sql.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.jdbc.core.*;
 import org.springframework.jdbc.core.namedparam.*;
+import org.springframework.stereotype.*;
 
 import dunkirk.reservation.dao.*;
 import dunkirk.reservation.domain.*;
 import dunkirk.reservation.sql.*;
 
+@Repository
 public class ProductDaoImpl implements ProductDao {
-	
+
 	private NamedParameterJdbcTemplate jdbc;
 	private RowMapper<Product> rowMapper = new BeanPropertyRowMapper<Product>(Product.class);
-	
+
 	@Autowired
 	public ProductDaoImpl(DataSource dataSource) {
 		super();
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
 	}
-
-
 
 	@Override
 	public List<Product> getList(int categoryId, int start) {
