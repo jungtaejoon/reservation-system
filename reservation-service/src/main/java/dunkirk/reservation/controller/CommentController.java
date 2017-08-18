22c85bc4d0ed70b922ec27224c086ff241cefed9
q941110 <q941110@hanmail.net>
2017-08-18 15:07:11 +0900

    comment controller 추가

---
+++ reservation-service/src/main/java/dunkirk/reservation/controller/CommentController.java
@@ -0,0 +1,15 @@
+package dunkirk.reservation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CommentController {
+	
+	@GetMapping("comment-write")
+	public String write(@RequestParam int reservationId){
+		return "reviewWrite";
+	}
+
+}
