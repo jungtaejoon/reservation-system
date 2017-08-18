31936a67e17e4dcd4217ff8e35d8c843cc4665d6
jungtaejoon <jungtaejoon@naver.com>
2017-08-18 19:25:33 +0900

    [FE] reservation 페이지 돌아가기 버튼 구현

--- reservation-service/src/main/webapp/resources/js/reserve/reserve.js
+++ reservation-service/src/main/webapp/resources/js/reserve/reserve.js
@@ -5,6 +5,8 @@ import jQuery from "jquery";
 window.$ = jQuery;
 
 $(function () {
+    $('a.btn_back').on('click', () => history.back());
+
     var tickets = []
     $(".qty").each(function(i, e){
         tickets.push(new Ticket($(e).attr('id')));
